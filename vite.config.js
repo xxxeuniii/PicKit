dd4cbb5ae41e0fc1bb286fe3afc90966b8246d97
srd13265058427 <1491372173@qq.com>
2025-08-28 16:35:02 +0800

    删除plugin

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import sitemapPlugin from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    sitemapPlugin({
-      hostname: 'https://pic-kit.vercel.app',
-      dynamicRoutes: [
-        '/',
-        '/compress',
-        '/crop',
-        '/convert',
-        '/rename'
-      ],
-      lastmod: new Date().toISOString(),
-      changefreq: 'weekly'
-    })
+
   ],
   resolve: {
     alias: {
