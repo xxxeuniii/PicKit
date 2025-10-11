cc0b707cafb016aa0df4484215ac434ad03c43c9
srd13265058427 <1491372173@qq.com>
2025-10-11 17:21:38 +0800

    pwa

--- src/main.js
+++ src/main.js
@@ -12,3 +12,11 @@ app.use(ElementPlus)
 app.use(router)
 app.use(i18n)
 app.mount('#app')
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js').catch((err) => {
+      console.error('SW registration failed:', err)
+    })
+  })
+}
